ac03da2ca0c0b01a79564eecf905f2995b510013
Ilya Shablouski <ilya.shablouski@ozitag.com>
2021-03-10 21:17:23 +0300

    add section img background

--- web/src/modules/Home/Home.tsx
+++ web/src/modules/Home/Home.tsx
@@ -4,18 +4,30 @@ import styled from 'styled-components';
 import Link from '@/components/Link';
 import { ReactComponent as OZiTAGLogo } from '@/assets/svg/ozitag-logo.svg';
 import Picture from '@/components/Picture';
+import infoBgUrl from '@/assets/images/home/intro/intro-bg.jpg';
+import infoBgUrl2x from '@/assets/images/home/intro/intro-bg@2x.jpg';
 import { colors } from '@/constants/theme';
 
 function Home() {
   return (
     <Section>
-      <span>WORK MIRACLES WITH US</span>
+      <Image>
+        <Picture
+          tabletLarge={{
+            src: infoBgUrl,
+            src2x: infoBgUrl2x,
+          }}
+        />
+      </Image>
+      <Inner>
+        <span>WORK MIRACLES WITH US</span>
+        <p>
+          A full-service digital agency with global capabilities across web
+          design and development, marketing and branding. We create digital
+          experiences that are human-centered and future proof.
+        </p>
+      </Inner>
 
-      <p>
-        A full-service digital agency with global capabilities across web design
-        and development, marketing and branding. We create digital experiences
-        that are human-centered and future proof.
-      </p>
       {/*<OZiTAGLogo />*/}
       {/*<Link to="/test">*/}
       {/*  <h2>Go to test</h2>*/}
@@ -29,9 +41,6 @@ function Home() {
 
 const Section = styled.section`
   position: relative;
-  padding: 313px 0 212px;
-  text-align: center;
-  color: ${colors.white};
 
   span {
     margin: 0 auto;
@@ -49,13 +58,40 @@ const Section = styled.section`
     font-size: 14px;
     line-height: 160%;
   }
-`;
 
-const Button = styled.button<{ isActive: boolean }>`
-  color: ${(props) => (props.isActive ? 'red' : 'green')};
-
-  &:last-child {
+  picture {
+    width: 100%;
   }
 `;
+
+const Image = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  z-index: 0;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const Inner = styled.div`
+  position: relative;
+  padding: 313px 0 212px;
+  text-align: center;
+  color: ${colors.white};
+`;
+
+// const Button = styled.button<{ isActive: boolean }>`
+//   color: ${(props) => (props.isActive ? 'red' : 'green')};
+//
+//   &:last-child {
+//   }
+// `;
 
 export default Home;
