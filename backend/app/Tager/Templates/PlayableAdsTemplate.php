64725aed9717490aeeb867ddb75c8e121cf40fbb
Vital Ozierski <v.ozierski@ozitag.com>
2021-03-10 16:53:57 +0300

    .env

---
+++ backend/app/Tager/Templates/PlayableAdsTemplate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tager\Templates;
+
+use OZiTAG\Tager\Backend\Pages\Structures\Template;
+
+class PlayableAdsTemplate extends Template
+{
+    public function __construct()
+    {
+        parent::__construct('Игровые рекламы', []);
+    }
+}
