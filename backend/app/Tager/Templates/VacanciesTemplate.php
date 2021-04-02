32d842781335ea50661acbbfae5fd9bb597c863a
Unrig <8295149-Unrig@users.noreply.gitlab.com>
2021-04-02 13:49:15 +0300

    Template

--- backend/app/Tager/Templates/VacanciesTemplate.php
+++ backend/app/Tager/Templates/VacanciesTemplate.php
@@ -2,12 +2,22 @@
 
 namespace App\Tager\Templates;
 
+use OZiTAG\Tager\Backend\Fields\Fields\ButtonField;
+use OZiTAG\Tager\Backend\Fields\Fields\GroupField;
+use OZiTAG\Tager\Backend\Fields\Fields\HtmlField;
+use OZiTAG\Tager\Backend\Fields\Fields\StringField;
 use OZiTAG\Tager\Backend\Pages\Structures\Template;
 
 class VacanciesTemplate extends Template
 {
     public function __construct()
     {
-        parent::__construct('Вакансии', []);
+        parent::__construct('Вакансии', [
+            new GroupField('Team Life', [
+                'teamLifeTitle' => new StringField('Название'),
+                'teamLifeDescription' => new HtmlField('Описание'),
+                'teamLifeButton' => new ButtonField('Кнопка'),
+            ]),
+        ]);
     }
 }
