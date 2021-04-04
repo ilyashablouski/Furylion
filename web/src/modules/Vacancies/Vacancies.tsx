7c72c827ea166ce269bd54b9309c10ce24a5e6c2
Ilya Shablouski <ilya.shablouski@ozitag.com>
2021-04-04 18:33:18 +0300

    add vacancies header structure

--- web/src/modules/Vacancies/Vacancies.tsx
+++ web/src/modules/Vacancies/Vacancies.tsx
@@ -1,22 +1,72 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import useCurrentPage from '@/hooks/useCurrentPage';
+import { colors } from '@/constants/theme';
+import { media } from '@/utils/mixin';
+import PlainPicture from '@/components/Picture';
 
 function Vacancies() {
   const page = useCurrentPage();
 
   return (
-    <Container>
-      {/*<h1>{page?.title + "I'm from pageModules"}</h1>*/}
-      {/*<div dangerouslySetInnerHTML={{ __html: page?.body ?? '' }} />*/}
-    </Container>
+    <HeaderSection>
+      <Inner>
+        <Title data-text="VACANCIES">Vacancies</Title>
+        <ImageContainer>
+          <PlainPicture />
+        </ImageContainer>
+      </Inner>
+    </HeaderSection>
   );
 }
 
-const Container = styled.div`
-  max-width: 1200px;
-  margin: 2rem auto;
+const HeaderSection = styled.section`
+  padding-top: 221px;
+  position: relative;
+  text-align: center;
+`;
+
+const Inner = styled.div`
+  display: inline-block;
+`;
+const Title = styled.span`
+  position: relative;
+  z-index: 1;
+  width: 100%;
+  font-weight: 900;
+  font-size: 225.58px;
+  line-height: 100%;
+  text-transform: uppercase;
+  color: ${colors.white};
+
+  ${media.desktop1366(css`
+    font-size: 16.5vw;
+  `)}
+
+  &:before {
+    content: attr(data-text);
+    position: absolute;
+    top: -13.1%;
+    left: 0;
+    transform-origin: bottom;
+    transform: rotateX(180deg);
+    line-height: 1em;
+    background: linear-gradient(
+      0deg,
+      rgba(255, 255, 255, 0.26) -0.06%,
+      rgba(155, 155, 155, 0) 37.65%
+    );
+    filter: blur(2px);
+    -webkit-background-clip: text;
+    color: transparent;
+  }
+`;
+
+const ImageContainer = styled.div`
+  position: absolute;
+  bottom: 13.7%;
+  right: 7.7%;
 `;
 
 export default Vacancies;
