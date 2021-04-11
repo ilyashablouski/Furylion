a4318b620420165422761868374bc185ea3b1cf3
Ilya Shablouski <ilya.shablouski@ozitag.com>
2021-04-11 22:43:16 +0300

    delete instagram feed plugin

--- web/src/modules/Vacancies/components/TeamLifeSection/components/InstagramFeed.tsx
+++ web/src/modules/Vacancies/components/TeamLifeSection/components/InstagramFeed.tsx
@@ -1,25 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import InstagramEmbed from 'react-instagram-embed';
 
 function InstagramFeed() {
-  return (
-    <Container>
-      <InstagramEmbed
-        url="https://www.instagram.com/p/CCPPq1Nl6uQ/"
-        clientAccessToken="355944969167741|960d85e71ec21bca49d502720e3c3649"
-        maxWidth={320}
-        hideCaption={true}
-        containerTagName="div"
-        protocol=""
-        injectScript
-        onLoading={() => {}}
-        onSuccess={() => {}}
-        onAfterRender={() => {}}
-        onFailure={() => {}}
-      />
-    </Container>
-  );
+  return <Container></Container>;
 }
 
 const Container = styled.div``;
