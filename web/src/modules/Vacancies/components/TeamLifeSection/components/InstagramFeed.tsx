0c24a0d6776784dbae1e02a0bfbff1d7eec4ee5e
Ilya Shablouski <ilya.shablouski@ozitag.com>
2021-04-16 11:11:46 +0300

    fix pipeline

--- web/src/modules/Vacancies/components/TeamLifeSection/components/InstagramFeed.tsx
+++ web/src/modules/Vacancies/components/TeamLifeSection/components/InstagramFeed.tsx
@@ -74,9 +74,9 @@ const Wrapper = styled.div`
 const ItemLink = styled(Link)`
   position: absolute;
   top: 0;
+  bottom: 0;
   right: 0;
   left: 0;
-  bottom: 0;
   width: 100%;
   height: 100%;
   z-index: 1;
