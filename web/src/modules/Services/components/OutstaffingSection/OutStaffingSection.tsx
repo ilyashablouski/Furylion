0fc5026717e5337ee75651fb3039d8ef690e454b
Ilya Shablouski <ilya.shablouski@ozitag.com>
2021-05-04 00:08:45 +0300

    add OutStaffingSection hover

--- web/src/modules/Services/components/OutstaffingSection/OutStaffingSection.tsx
+++ web/src/modules/Services/components/OutstaffingSection/OutStaffingSection.tsx
@@ -7,8 +7,14 @@ import { colors } from '@/constants/theme';
 import ContentContainer from '@/components/ContentContainer';
 import { media } from '@/utils/mixin';
 
+import TagsElement from './components/TagsElement';
+
 function OutStaffingSection() {
-  const [isShown, setIsShown] = useState(false);
+  const [isChangedColor, setChangedColor] = useState(false);
+
+  function onChangeBackgroundColor() {
+    setChangedColor(!isChangedColor);
+  }
 
   const page = useCurrentPage<OutStaffingSectionType>();
   if (!page) return null;
@@ -16,7 +22,7 @@ function OutStaffingSection() {
   const pageFields = page.templateFields;
 
   return (
-    <Wrapper className="section-wrapper">
+    <Wrapper isChangedColor={isChangedColor}>
       <ContentContainer>
         <Title>{pageFields.outStaffingTitle}</Title>
 
@@ -38,29 +44,12 @@ function OutStaffingSection() {
                   ? outStaffingItem.tags.split(',')
                   : [];
                 return (
-                  <TagsInner key={index} isShown={isShown}>
-                    <TagsContainer>
-                      <TagsBlock
-                        onMouseEnter={() => setIsShown(true)}
-                        onMouseLeave={() => setIsShown(false)}
-                      >
-                        <TagsTitle>{outStaffingItem.title}</TagsTitle>
-                        <TagsWrapper>
-                          <Tags>
-                            {tagsArray
-                              ? tagsArray.map((tag, index) => {
-                                  return (
-                                    <TagItem key={index}>
-                                      <Tag>{tag}</Tag>
-                                    </TagItem>
-                                  );
-                                })
-                              : null}
-                          </Tags>
-                        </TagsWrapper>
-                      </TagsBlock>
-                    </TagsContainer>
-                  </TagsInner>
+                  <TagsElement
+                    key={index}
+                    tagsArray={tagsArray}
+                    title={outStaffingItem.title}
+                    onChangeBackgroundColor={onChangeBackgroundColor}
+                  />
                 );
               })
             : null}
@@ -70,12 +59,19 @@ function OutStaffingSection() {
   );
 }
 
-const Wrapper = styled.section`
+const Wrapper = styled.section<{ isChangedColor: boolean }>`
   padding: 100px 0;
+  transition: all 150ms ease-in-out;
 
   ${media.tabletSmall(css`
     padding: 70px 0;
   `)}
+
+  ${(props) =>
+    props.isChangedColor &&
+    css`
+      background: ${colors.red};
+    `}}
 `;
 
 const Title = styled.span`
@@ -92,6 +88,7 @@ const Title = styled.span`
     font-size: 32px;
   `)}
 `;
+
 const TextContainer = styled.div`
   margin-top: 25px;
   margin-left: -10px;
@@ -162,111 +159,6 @@ const MainWrapper = styled.div`
     display: block;
     width: 100%;
   `)}
-`;
-
-const TagsContainer = styled.div`
-  max-width: 1368px;
-  width: 100%;
-  margin: 0 auto;
-  padding: 0 68px;
-  
-  ${media.tabletSmall(css`
-    padding: 0;
-    height: 100%;
-  `)} 
-}
-`;
-
-const TagsInner = styled.div<{ isShown: boolean }>`
-  margin-top: 25px;
-  transition: all 150ms ease-in-out;
-
-  ${media.tabletSmall(css`
-    margin: 20px 10px 0;
-    flex: 1 1 calc(50% - 20px);
-    max-width: calc(50% - 20px);
-  `)}
-
-  ${media.mobile(css`
-    margin: 10px 0 0;
-    max-width: none;
-  `)}
-`;
-
-const TagsBlock = styled.div`
-  display: inline-flex;
-  align-items: center;
-  border: 1px dashed rgba(194, 25, 30, 0.8);
-
-  ${media.tabletSmall(css`
-    padding: 20px 25px;
-    height: 100%;
-    display: block;
-  `)}
-`;
-
-const TagsTitle = styled.div`
-  margin-left: 25px;
-  padding: 10px 0;
-  flex: 0 0 178px;
-  font-weight: 900;
-  font-size: 24px;
-  line-height: 130%;
-  color: rgba(255, 255, 255, 0.8);
-
-  ${media.tabletSmall(css`
-    margin-left: 0;
-    padding: 0;
-  `)}
-`;
-const TagsWrapper = styled.div`
-  padding: 25px;
-  display: inline-grid;
-  width: 100%;
-
-  ${media.tabletSmall(css`
-    margin-top: 20px;
-    padding: 0;
-    display: block;
-  `)}
-`;
-
-const Tags = styled.div`
-  display: flex;
-  overflow: auto;
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
-
-  ${media.tabletSmall(css`
-    display: block;
-    overflow: initial;
-  `)}
-`;
-
-const TagItem = styled.div`
-  &:not(:first-child) {
-    margin-left: 25px;
-
-    ${media.tabletSmall(css`
-      margin-top: 10px;
-      margin-left: 0;
-    `)}
-  }
-`;
-
-const Tag = styled.span`
-  padding: 10px;
-  display: inline-block;
-  background: rgba(255, 255, 255, 0.8);
-  color: ${colors.dark};
-  font-weight: normal;
-  font-size: 16px;
-  line-height: 130%;
-  white-space: nowrap;
 `;
 
 export default OutStaffingSection;
