c72076c88adbe0da59549ea3c5bc217ceb7c99a9
PavelSerebryany <pavel97107@gmail.com>
2021-05-21 14:58:41 +0300

    update gallery

--- web/src/modules/PlayableAds/components/Gallery/Gallery.tsx
+++ web/src/modules/PlayableAds/components/Gallery/Gallery.tsx
@@ -1,81 +1,105 @@
-import React, { useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
+import React, { useRef } from 'react';
+import styled, { css } from 'styled-components';
 
 import { convertThumbnailToPictureImage } from '@tager/web-modules';
 
 import ContentContainer from '@/components/ContentContainer';
 import { AdsHeadItemType } from '@/typings/model';
 import Picture from '@/components/Picture';
+import { media } from '@/utils/mixin';
 
 interface Props {
   isRevert?: boolean;
   itemList: Array<AdsHeadItemType>;
 }
 
-function Gallery({ isRevert = false, itemList }: Props) {
-  const wrapperRef = useRef<HTMLDivElement>(null);
-  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+const transition = 'transform .2s';
 
-  useEffect(() => {
-    console.log(itemList);
-  });
+function getDifferenceCoordinate<T extends number>(
+  leftCoordinateWithCurrentWidth: T,
+  leftCoordinateOnRightBlock: T
+): number {
+  if (leftCoordinateWithCurrentWidth > leftCoordinateOnRightBlock) {
+    return leftCoordinateWithCurrentWidth - leftCoordinateOnRightBlock;
+  } else {
+    return 0;
+  }
+}
+
+function Gallery({ isRevert = false, itemList }: Props) {
+  const contentRef = useRef<HTMLDivElement>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
 
   function handleMouseMove(event: React.MouseEvent) {
-    const contentElement = wrapperRef.current;
+    const contentElement = contentRef.current;
     if (contentElement) {
       const content = contentElement.getBoundingClientRect();
       const value =
         event.clientX - (content.left + Math.floor(content.width / 2));
       const optionValue = isRevert ? String(value) : String(-value);
       contentElement.style.setProperty('--x', optionValue);
+      contentElement.style.setProperty('--transition', 'initial');
+      if (wrapperRef.current?.getBoundingClientRect().left) {
+      }
     }
   }
 
-  function handleMouseOverOnCard(
-    index: number,
+  function handleMouseLeave(
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) {
-    // if (event.target instanceof HTMLDivElement) {
-    //   const coordinate = event.target.getBoundingClientRect();
-    //   console.log(coordinate);
-    // }
-    return () => setActiveIndex(index);
+    if (event.currentTarget instanceof HTMLDivElement) {
+      event.currentTarget.style.paddingLeft = '0px';
+    }
+    contentRef.current?.style.setProperty('--transition', transition);
+    contentRef.current?.style.setProperty('--x', '0');
   }
 
-  function handleMouseLeaveOnCard() {
-    setActiveIndex(null);
-  }
+  function onMouseOverOnCard(
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) {
+    if (event.currentTarget instanceof HTMLDivElement) {
+      const wrapperCoordinate = wrapperRef.current?.getBoundingClientRect();
+      const wrapperLeft = wrapperCoordinate?.left ?? 0;
+      const wrapperWidth = wrapperCoordinate?.width ?? 0;
 
-  function handleMouseLeave() {
-    wrapperRef.current?.style.setProperty('--x', '0');
+      const currentTargetCoordinate = event.currentTarget.getBoundingClientRect();
+
+      const currentTargetLeft = currentTargetCoordinate.left ?? 0;
+      const currentTargetWidth = currentTargetCoordinate.width ?? 0;
+
+      const leftCoordinateOnRightBlock = wrapperLeft + wrapperWidth;
+      const leftCoordinateWithCurrentWidth =
+        currentTargetLeft + currentTargetWidth;
+
+      const resultDifference = getDifferenceCoordinate(
+        leftCoordinateWithCurrentWidth,
+        leftCoordinateOnRightBlock
+      );
+
+      if (currentTargetLeft < wrapperLeft) {
+        const result = wrapperLeft - currentTargetLeft;
+        event.currentTarget.style.marginLeft = `${result}px`;
+      } else if (resultDifference) {
+        event.currentTarget.style.marginRight = `${resultDifference}px`;
+      }
+    }
   }
 
   return (
-    <Component>
+    <Component ref={wrapperRef}>
       <ContentContainer>
-        <Wrapper
-          ref={wrapperRef}
-          onMouseMove={handleMouseMove}
-          onMouseLeave={handleMouseLeave}
-          isRevert={isRevert}
-        >
-          <Content>
+        <Wrapper isRevert={isRevert}>
+          <Content
+            ref={contentRef}
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}
+          >
             {itemList.map((item, index) => {
-              const style =
-                activeIndex !== null
-                  ? activeIndex > index
-                    ? '-40px'
-                    : activeIndex === index
-                    ? 'initial'
-                    : '40px'
-                  : 'initial';
-
               return (
                 <Card
+                  onMouseOver={onMouseOverOnCard}
+                  data-index={index}
                   key={index}
-                  xPixels={style}
-                  onMouseOver={(event) => handleMouseOverOnCard(index, event)}
-                  onMouseLeave={handleMouseLeaveOnCard}
                 >
                   <Picture {...convertThumbnailToPictureImage(item.image)} />
                 </Card>
@@ -94,38 +118,57 @@ const Component = styled.div`
 
 const Wrapper = styled.div<{ isRevert: boolean }>`
   transform: translateX(${({ isRevert }) => (isRevert ? '-30%' : '-20%')});
+
+  ${media.mobileMedium(css`
+    transform: translateX(-88%);
+  `)}
 `;
-const Card = styled.div<{ xPixels: '-40px' | '40px' | 'initial' }>`
+const Card = styled.div`
   flex: 1 1 100%;
   width: 100%;
   padding-right: 30px;
-  border-radius: 22px;
-  transition: transform 0.2s linear;
-  transform: translateX(${({ xPixels }) => xPixels});
+
+  transition: all 0.2s linear;
+
+  & > div {
+    overflow: hidden;
+    border-radius: 22px;
+  }
 
   &:last-child {
     margin-right: 0;
   }
 
   &:hover {
-    transition: transform 0.2s linear;
+    transition: all 0.2s linear;
     transform: scale(1.1);
+    padding: 0 60px 0 30px;
   }
 
   img {
-    transition: transform 0.3s linear !important;
     object-fit: cover;
     cursor: pointer;
     max-width: initial;
+
+    ${media.mobile(css`
+      max-width: 294px;
+      height: 165px;
+    `)}
+    ${media.mobileMedium(css`
+      width: 288px;
+      height: 160px;
+    `)}
   }
+
+  ${media.mobile(css`
+    padding-right: 15px;
+  `)}
 `;
 const Content = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  transition: transform 0.3s linear;
-
-  transform: translateX(calc(var(--x) / 3 * 1px));
+  transform: translateX(calc(var(--x) / 3 * 0.5px));
 `;
 
 export default Gallery;
