c36816adb83b0ea809d679f65713c7bec37d4aee
Vital Ozierski <v.ozierski@ozitag.com>
2021-06-12 16:05:20 +0300

    add lazy

--- backend/config/filestorage.php
+++ backend/config/filestorage.php
@@ -72,7 +72,7 @@ return [
             'storage' => ConfigHelper::fileStorage('home-page'),
             'validator' => ConfigHelper::imageValidator(),
             'thumbnails' => [
-                'mobile' => ConfigHelper::thumbWithWebpAnd2x(768),
+                'mobile' => ConfigHelper::thumbWithWebpAnd2x(420),
                 'tablet' => ConfigHelper::thumbWithWebpAnd2x(1024),
                 'laptop' => ConfigHelper::thumbWithWebpAnd2x(1260),
                 'desktop' => ConfigHelper::thumbWithWebpAnd2x(1400),
