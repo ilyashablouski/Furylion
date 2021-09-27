fec2b02eb2a204bbde0c899df004cbdfba4cba4b
Vital Ozierski <v.ozierski@ozitag.com>
2021-09-27 19:02:03 +0300

    update

--- backend/app/Jobs/Web/GetCoursesCountJob.php
+++ backend/app/Jobs/Web/GetCoursesCountJob.php
@@ -4,11 +4,17 @@ namespace App\Jobs\Web;
 
 use App\Repositories\VacancyRepository;
 use OZiTAG\Tager\Backend\Core\Jobs\Job;
+use OZiTAG\Tager\Backend\Pages\Repositories\PagesRepository;
 
 class GetCoursesCountJob extends Job
 {
-    public function handle(VacancyRepository $vacancyRepository)
+    public function handle(PagesRepository $pagesRepository)
     {
-        return $vacancyRepository->count();
+        $rootPage = $pagesRepository->findByTemplate('courses')->first();
+        if(!$rootPage){
+            return 0;
+        }
+
+        return $pagesRepository->builder()->where('parent_id', $rootPage->id)->count();
     }
 }
