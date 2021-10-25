617a3e0d4b284ed505ba1d1bf32d715348b2e157
Vital Ozierski <v.ozierski@ozitag.com>
2021-10-25 20:53:55 +0300

    add api endpoints

--- web/src/modules/Courses/components/Projects/Card/ProjectsCard.tsx
+++ web/src/modules/Courses/components/Projects/Card/ProjectsCard.tsx
@@ -8,12 +8,7 @@ import Picture from '@/components/Picture';
 function ProjectsCard({ url, url_webp, url_2x, url_webp_2x }: ThumbnailType) {
   return (
     <Component>
-      <Image
-        src={url}
-        src2x={url_webp}
-        srcWebp={url_2x}
-        srcWebp2x={url_webp_2x}
-      />
+      <Image src={url} src2x={url_2x} srcWebp={null} srcWebp2x={null} />
     </Component>
   );
 }
