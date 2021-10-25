7627aa3c20e1a15b1f40a67e0080f85cacb6cd51
Vital Ozierski <v.ozierski@ozitag.com>
2021-10-25 20:11:47 +0300

    add api endpoints

--- backend/app/Jobs/Web/GetCoursesCountJob.php
+++ backend/app/Jobs/Web/GetCoursesCountJob.php
@@ -4,17 +4,31 @@ namespace App\Jobs\Web;
 
 use App\Repositories\VacancyRepository;
 use OZiTAG\Tager\Backend\Core\Jobs\Job;
+use OZiTAG\Tager\Backend\Pages\Models\TagerPageField;
+use OZiTAG\Tager\Backend\Pages\Repositories\PageFieldFilesRepository;
 use OZiTAG\Tager\Backend\Pages\Repositories\PagesRepository;
 
 class GetCoursesCountJob extends Job
 {
-    public function handle(PagesRepository $pagesRepository)
+    public function handle(PagesRepository $pagesRepository, PageFieldFilesRepository $pageFieldFilesRepository)
     {
         $rootPage = $pagesRepository->findByTemplate('courses')->first();
-        if(!$rootPage){
+        if (!$rootPage) {
             return 0;
         }
 
-        return $pagesRepository->builder()->where('parent_id', $rootPage->id)->count();
+        /** @var TagerPageField $field */
+        $field = $pageFieldFilesRepository->builder()->where('page_id', '=', $rootPage->id)
+            ->where('field', '=', 'coursesItems')->first();
+        if (!$field) {
+            return 0;
+        }
+
+        $fieldValue = $field->value ? json_decode($field->value, true) : null;
+        if (!$fieldValue) {
+            return 0;
+        }
+
+        return count($fieldValue);
     }
 }
